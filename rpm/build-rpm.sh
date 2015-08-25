49d0cb1672884b656f5aeb5c7bd3be6c129d1492
Amit Bose <bose@noironetworks.com>
2015-08-25 15:21:51 -0700

    Bump package version to Kilo (2015.1)
    Also modified the package creation scripts to handle 'dev'
    versioning correctly.
    
    Signed-off-by: Amit Bose <bose@noironetworks.com>

--- rpm/build-rpm.sh
+++ rpm/build-rpm.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 # Should be run from the root of the source tree
 
+if [ ! -d rpm ]; then
+   echo "Directory 'rpm' not found"
+   exit 1
+fi
+SPEC_FILE_IN=`ls rpm/*.spec.in`
+if [ -z $SPEC_FILE_IN ]; then
+   echo "RPM spec file not found"
+   exit 1
+fi
 BUILD_DIR=${BUILD_DIR:-`pwd`/rpmbuild}
 mkdir -p $BUILD_DIR/BUILD $BUILD_DIR/SOURCES $BUILD_DIR/SPECS $BUILD_DIR/RPMS $BUILD_DIR/SRPMS
+NAME=`python setup.py --name`
 RELEASE=${RELEASE:-1}
-VERSION=`python setup.py --version`
-SPEC_FILE=neutron-ml2-driver-apic.spec
-sed -e "s/@VERSION@/$VERSION/" -e "s/@RELEASE@/$RELEASE/" rpm/$SPEC_FILE.in > $BUILD_DIR/SPECS/$SPEC_FILE
+VERSION_PY=`python setup.py --version`
+VERSION=${VERSION_PY/.dev/~dev}
+SPEC_FILE=${SPEC_FILE_IN/.in/}
+SPEC_FILE=${SPEC_FILE/rpm\//}
+sed -e "s/@VERSION@/$VERSION/" \
+    -e "s/@VERSION_PY@/$VERSION_PY/" \
+    -e "s/@RELEASE@/$RELEASE/" \
+    $SPEC_FILE_IN > $BUILD_DIR/SPECS/$SPEC_FILE
 python setup.py sdist --dist-dir $BUILD_DIR/SOURCES
+mv $BUILD_DIR/SOURCES/$NAME-$VERSION_PY.tar.gz $BUILD_DIR/SOURCES/$NAME-$VERSION.tar.gz
 rpmbuild --clean -ba --define "_topdir $BUILD_DIR" $BUILD_DIR/SPECS/$SPEC_FILE
