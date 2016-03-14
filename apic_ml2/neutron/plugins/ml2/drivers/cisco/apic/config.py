e513e755590adc95499ea041742f31ed16665c6f
Mandeep Dhami <dhami@noironetworks.com>
2016-03-14 10:04:23 -0700

    Changing default for single_tenant_mode to False

--- apic_ml2/neutron/plugins/ml2/drivers/cisco/apic/config.py
+++ apic_ml2/neutron/plugins/ml2/drivers/cisco/apic/config.py
@@ -89,7 +89,7 @@ apic_opts = [
                 help=_("If True, one L3 CTX per tenant will be created "
                        "instead of a global one. This will enable overlapping "
                        "IPs across tenants (but not within the same one).")),
-    cfg.BoolOpt('single_tenant_mode', default=True,
+    cfg.BoolOpt('single_tenant_mode', default=False,
                 help=_("All the Openstack tenants will be described by a "
                        "single ACI tenant.")),
     cfg.StrOpt('network_constraints_filename',
