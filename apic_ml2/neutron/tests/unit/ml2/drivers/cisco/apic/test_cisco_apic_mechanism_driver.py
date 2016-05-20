d01276a97a36e2c81f9849eb5b3e43e425464b0f
Amit Bose <amitbose@gmail.com>
2016-05-19 18:09:16 -0700

    Fix multiple router delete failure
    Calling 'get_router_contract' during gateway-port delete
    can inadvertently create an entry for the router in
    table cisco_ml2_apic_contracts. This leads to a stray
    entry when a router is being deleted with gateway-set.
    
    Fixes #102
    
    Signed-off-by: Amit Bose <amitbose@gmail.com>
    (cherry picked from commit c61304fad8aaa92fabbb1bbc296700bcdb56dd5d)
    (cherry picked from commit a538c2752211683a625cc5c07c2d75ea7ca078e2)

--- apic_ml2/neutron/tests/unit/ml2/drivers/cisco/apic/test_cisco_apic_mechanism_driver.py
+++ apic_ml2/neutron/tests/unit/ml2/drivers/cisco/apic/test_cisco_apic_mechanism_driver.py
@@ -2512,8 +2512,6 @@ tt':
 
         self.driver._delete_path_if_last = mock.Mock()
         mgr = self.driver.apic_manager
-        mgr.get_router_contract.return_value = mocked.FakeDbContract(
-            mocked.APIC_CONTRACT + 'r1')
 
         # Delete first GW port
         self.driver.delete_port_postcommit(gw_ports[0])
@@ -2523,12 +2521,12 @@ tt':
                 "Shd-%s" % self._scoped_name(ext_net_name, tenant='t1'))
         exp_calls = [
             mock.call(shadow_l3out,
-                      mgr.get_router_contract.return_value,
+                      'contract-r1',
                       external_epg=shadow_ext_epg,
                       owner=self._tenant(ext_nat=True, neutron_tenant='t1'),
                       provided=True),
             mock.call(shadow_l3out,
-                      mgr.get_router_contract.return_value,
+                      'contract-r1',
                       external_epg=shadow_ext_epg,
                       owner=self._tenant(ext_nat=True, neutron_tenant='t1'),
                       provided=False)
@@ -2539,8 +2537,6 @@ tt':
 
         # Delete second GW port
         mgr.unset_contract_for_external_epg.reset_mock()
-        mgr.get_router_contract.return_value = mocked.FakeDbContract(
-            mocked.APIC_CONTRACT + 'r2')
         self.driver.delete_port_postcommit(gw_ports[0])
 
         if self.driver.per_tenant_context:
@@ -2555,11 +2551,11 @@ tt':
 
             exp_calls = [
                 mock.call(shadow_l3out,
-                          mgr.get_router_contract.return_value,
+                          'contract-r2',
                           external_epg=shadow_ext_epg,
                           owner=self._tenant(ext_nat=True), provided=True),
                 mock.call(shadow_l3out,
-                          mgr.get_router_contract.return_value,
+                          'contract-r2',
                           external_epg=shadow_ext_epg,
                           owner=self._tenant(ext_nat=True), provided=False)
             ]
