98ce7673ead05f5cd850057d3638ae85d4401db6
Kent Wu <wu@noironetworks.com>
2017-01-11 11:54:16 -0800

    Use neutron rpc TRANSPORT instead
    neutron rpc TRANSPORT has taken the alias into account.
    
    (cherry picked from commit c416442fe84df6cf82e3d7a31254a7327b0514c8)

--- apic_ml2/neutron/plugins/ml2/drivers/cisco/apic/mechanism_apic.py
+++ apic_ml2/neutron/plugins/ml2/drivers/cisco/apic/mechanism_apic.py
@@ -534,14 +534,14 @@ class APICMechanismDriver(api.MechanismDriver,
         return self.opflex_conn.consume_in_threads()
 
     def _setup_keystone_notification_listeners(self):
-        transport = oslo_messaging.get_transport(cfg.CONF)
         targets = [oslo_messaging.Target(
             exchange=self.keystone_notification_exchange,
             topic=self.keystone_notification_topic, fanout=True)]
         endpoints = [KeystoneNotificationEndpoint(self)]
         pool = "cisco_ml2_listener-workers"
         server = oslo_messaging.get_notification_listener(
-            transport, targets, endpoints, executor='eventlet', pool=pool)
+            n_rpc.TRANSPORT, targets, endpoints,
+            executor='eventlet', pool=pool)
         server.start()
 
     def _setup_topology_rpc_listeners(self):
